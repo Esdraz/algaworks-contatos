ac56df05f61eb1169a8cc7e062bf1b96c6139b55
Francisco <fsousa.jpa@gmail.com>
2026-01-14 11:29:07 -0300

    feat: cria o controller de contatos

---
+++ src/main/java/mini/projeto/de/cadastro/de/contatos/ContatosController.java
@@ -0,0 +1,24 @@
+package mini.projeto.de.cadastro.de.contatos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+
+@Controller
+public class ContatosController {
+
+    private static final ArrayList<Contato> LISTA_CONTATOS = new ArrayList<>();
+    static {
+        LISTA_CONTATOS.add(new Contato("1", "Maria", "+55 34 99876-5432"));
+        LISTA_CONTATOS.add(new Contato("2", "João", "+55 34 91234-5678"));
+        LISTA_CONTATOS.add(new Contato("3", "Pedro", "+55 34 99999-8888"));
+        LISTA_CONTATOS.add(new Contato("4", "Ana", "+55 34 97777-6666"));
+        LISTA_CONTATOS.add(new Contato("5", "Carlos", "+55 34 95555-4444"));
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
